b231ee204506935c5fb428dd3a0391255b779be6
DESKTOP-TDSET4L\eduar <eduardo@daccord.com.br>
2024-10-07 17:51:26 -0300

    feat: Adds comment to song list adapter

--- app/src/main/java/com/example/lessonsapp/SongListAdapter.kt
+++ app/src/main/java/com/example/lessonsapp/SongListAdapter.kt
@@ -10,19 +10,28 @@ import android.widget.ImageView
 import android.widget.TextView
 
 class SongListAdapter(private val context: Context, private val list: List<SongModel>) : ArrayAdapter<SongModel>(context, R.layout.it_song_list, list) {
+
     @SuppressLint("ViewHolder")
     override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
 
+        // Criando a view (layout) que vamos colocar os dados do elemento[i]
         val view = LayoutInflater.from(context).inflate(R.layout.it_song_list, parent, false)
 
+        // pegando o elemento[i] atual da lista
         val song = list[position]
 
+        // Acessando as views do layout através dos IDs
         val songCover: ImageView = view.findViewById(R.id.ivSongCover)
         val songTitle: TextView = view.findViewById(R.id.tvSongTitle)
-        val songArtist: TextView =view.findViewById(R.id.tvSongArtist)
+        val songArtist: TextView = view.findViewById(R.id.tvSongArtist)
 
+        // colocando a imagem do elemento[i] da lista no ImageView
         songCover.setImageResource(song.image)
+
+        // colocando o titulo do elemento[i] da lista no TextView
         songTitle.text = song.title
+
+        // colocando o artista do elemento[i] da lista no Textview
         songArtist.text = song.artist
 
         return view
